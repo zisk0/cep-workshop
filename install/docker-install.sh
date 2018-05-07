564f812f6156f1309ed73102f43a3549e44b85d8
zisk0 <pako.gam@gmail.com>
2018-05-07 20:59:58 +0200

    Add .gitignore

---
+++ install/docker-install.sh
@@ -0,0 +1,16 @@
+apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+apt-get update
+apt-get install docker-ce
